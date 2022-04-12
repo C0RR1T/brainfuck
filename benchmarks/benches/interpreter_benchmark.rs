1deadfc60597ae63db5090cae081c6becae57eb6
CORR1T <corsin@ragett.li>
2022-04-12 14:58:10 +0200

    New release: v0.2.0

--- benchmarks/benches/interpreter_benchmark.rs
+++ benchmarks/benches/interpreter_benchmark.rs
@@ -2,23 +2,24 @@ extern crate interpreter;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use interpreter::Interpreter;
 use parser::hello_world;
 
 fn interpreter(c: &mut Criterion) {
     c.bench_function("interpret hello world", |b| {
-        b.iter(|| interpreter::Interpreter::new().interpret(&hello_world()))
+        b.iter(|| {
+            interpreter::Interpreter::new().interpret(&hello_world());
+        })
     });
     c.bench_function("interpret file fizzbuzz", |b| {
         b.iter(|| {
             interpreter::Interpreter::new()
-                .interpret_file("../brainfuck-example/fizzbuzz.bf".to_string())
+                .interpret_file_quiet("../brainfuck-example/fizzbuzz.txt".to_string());
         })
     });
     c.bench_function("interpret file hello-world", |b| {
         b.iter(|| {
             interpreter::Interpreter::new()
-                .interpret_file("../brainfuck-example/hello-world.bf".to_string())
+                .interpret_file_quiet("../brainfuck-example/hello-world.txt".to_string());
         })
     });
 }
