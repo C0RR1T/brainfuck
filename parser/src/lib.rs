2256e2fc41cee557cd05da98b62d84d517622679
CORR1T <corsin@ragett.li>
2022-06-13 15:07:46 +0200

    Remove optimizations

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -18,13 +18,12 @@ pub enum ParserError {
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Instruction {
     Loop(Vec<Instruction>),
-    Add(u8),
-    Subtract(u8),
-    Left(u8),
-    Right(u8),
-    Clear,
-    Output,
+    Add,
+    Subtract,
+    Left,
+    Right,
     Input,
+    Output,
 }
 
 pub struct Parser {
@@ -61,21 +60,19 @@ impl Parser {
             LexerToken {
                 token: TokenType::Add,
                 ..
-            } => Ok(Instruction::Add(self.combine_token(TokenType::Add))),
+            } => Ok(Instruction::Add),
             LexerToken {
                 token: TokenType::Subtract,
                 ..
-            } => Ok(Instruction::Subtract(
-                self.combine_token(TokenType::Subtract),
-            )),
+            } => Ok(Instruction::Subtract),
             LexerToken {
                 token: TokenType::Left,
                 ..
-            } => Ok(Instruction::Left(self.combine_token(TokenType::Left))),
+            } => Ok(Instruction::Left),
             LexerToken {
                 token: TokenType::Right,
                 ..
-            } => Ok(Instruction::Right(self.combine_token(TokenType::Right))),
+            } => Ok(Instruction::Right),
             LexerToken {
                 token: TokenType::Output,
                 ..
@@ -87,24 +84,8 @@ impl Parser {
         }
     }
 
-    fn combine_token(&mut self, token_to_match: TokenType) -> u8 {
-        let mut amount = 1;
-        while let Some(token) = self.peek_nth((amount - 1) as usize) {
-            if token.token == token_to_match && amount < u8::MAX {
-                amount += 1;
-            } else {
-                break;
-            }
-        }
-        if amount > 1 {
-            self.consume_elements((amount - 1) as usize);
-        }
-        amount
-    }
-
     fn parse_loop(&mut self, first_token: &LexerToken) -> ParserResult<Instruction> {
         let mut loop_instructions = Vec::new();
-        let mut is_clearing_cell = true;
         while let Some(token) = self.next() {
             match token {
                 LexerToken {
@@ -114,20 +95,9 @@ impl Parser {
 
                 token => {
                     let new_token = self.parse_token(&token)?;
-                    if is_clearing_cell {
-                        match new_token {
-                            Instruction::Add(_) | Instruction::Subtract(_) | Instruction::Clear => {
-                                is_clearing_cell = true
-                            }
-                            _ => is_clearing_cell = false,
-                        }
-                    }
                     loop_instructions.push(new_token);
                 }
             }
-        }
-        if is_clearing_cell {
-            return Ok(Instruction::Clear);
         }
 
         Err(UnexpectedEOF(Span::from(
@@ -165,11 +135,7 @@ fn parser_test() {
         ])
         .parse()
         .unwrap(),
-        vec![
-            Instruction::Left(1),
-            Instruction::Right(1),
-            Instruction::Add(1)
-        ]
+        vec![Instruction::Left, Instruction::Right, Instruction::Add]
     );
     assert_eq!(
         Parser::new(vec![
@@ -288,62 +254,62 @@ fn parser_test() {
 
 pub fn hello_world() -> Vec<Instruction> {
     vec![
-        Instruction::Add(8),
+        Instruction::Add,
         Instruction::Loop(vec![
-            Instruction::Right(1),
-            Instruction::Add(4),
+            Instruction::Right,
+            Instruction::Add,
             Instruction::Loop(vec![
-                Instruction::Right(1),
-                Instruction::Add(2),
-                Instruction::Right(1),
-                Instruction::Add(3),
-                Instruction::Right(1),
-                Instruction::Add(3),
-                Instruction::Right(1),
-                Instruction::Add(1),
-                Instruction::Left(4),
-                Instruction::Subtract(1),
+                Instruction::Right,
+                Instruction::Add,
+                Instruction::Right,
+                Instruction::Add,
+                Instruction::Right,
+                Instruction::Add,
+                Instruction::Right,
+                Instruction::Add,
+                Instruction::Left,
+                Instruction::Subtract,
             ]),
-            Instruction::Right(1),
-            Instruction::Add(1),
-            Instruction::Right(1),
-            Instruction::Add(1),
-            Instruction::Right(1),
-            Instruction::Subtract(1),
-            Instruction::Right(2),
-            Instruction::Add(1),
-            Instruction::Loop(vec![Instruction::Left(1)]),
-            Instruction::Left(1),
-            Instruction::Subtract(1),
+            Instruction::Right,
+            Instruction::Add,
+            Instruction::Right,
+            Instruction::Add,
+            Instruction::Right,
+            Instruction::Subtract,
+            Instruction::Right,
+            Instruction::Add,
+            Instruction::Loop(vec![Instruction::Left]),
+            Instruction::Left,
+            Instruction::Subtract,
         ]),
-        Instruction::Right(2),
+        Instruction::Right,
         Instruction::Output,
-        Instruction::Right(1),
-        Instruction::Subtract(3),
+        Instruction::Right,
+        Instruction::Subtract,
         Instruction::Output,
-        Instruction::Add(7),
+        Instruction::Add,
         Instruction::Output,
         Instruction::Output,
-        Instruction::Add(3),
+        Instruction::Add,
         Instruction::Output,
-        Instruction::Right(2),
+        Instruction::Right,
         Instruction::Output,
-        Instruction::Left(1),
-        Instruction::Subtract(1),
+        Instruction::Left,
+        Instruction::Subtract,
         Instruction::Output,
-        Instruction::Left(1),
+        Instruction::Left,
         Instruction::Output,
-        Instruction::Add(3),
+        Instruction::Add,
         Instruction::Output,
-        Instruction::Subtract(6),
+        Instruction::Subtract,
         Instruction::Output,
-        Instruction::Subtract(8),
+        Instruction::Subtract,
         Instruction::Output,
-        Instruction::Right(2),
-        Instruction::Add(1),
+        Instruction::Right,
+        Instruction::Add,
         Instruction::Output,
-        Instruction::Right(1),
-        Instruction::Add(2),
+        Instruction::Right,
+        Instruction::Add,
         Instruction::Output,
     ]
 }
